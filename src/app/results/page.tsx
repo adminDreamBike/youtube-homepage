71ea9549f32414427bcf23485660a9c9965335f9
Carlos Rodriguez <carlos.dumar.con@greatminds.com>
2024-09-24 18:06:24 -0500

    youtube homepage added 4

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -2,8 +2,9 @@
 
 import { Filter } from "@/components/Filter/Filter";
 import { VideoList } from "@/components/VideoList/VideoList";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Skeleton } from "@chakra-ui/react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
@@ -11,9 +12,11 @@ export default function Page() {
   const URL_SEARCH_VIDEOS = "https://www.googleapis.com/youtube/v3/search";
 
   return (
-    <Flex flexDirection="column" gap="20px">
-      <Filter />
-      <VideoList url={URL_SEARCH_VIDEOS} q={q_param} type="video" />
-    </Flex>
+    <Suspense fallback={<Skeleton />}>
+      <Flex flexDirection="column" gap="20px">
+        <Filter />
+        <VideoList url={URL_SEARCH_VIDEOS} q={q_param} type="video" />
+      </Flex>
+    </Suspense>
   );
 }

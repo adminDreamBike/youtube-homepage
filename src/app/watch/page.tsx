66f4a9c9e837d4b205149803f61d1356af9009e5
Carlos Rodriguez <carlos.dumar.con@greatminds.com>
2024-09-24 18:03:10 -0500

    youtube homepage added 3

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -9,6 +9,7 @@ import {
   IconButton,
   Button,
   Badge,
+  Skeleton,
 } from "@chakra-ui/react";
 import { useSearchParams } from "next/navigation";
 import NextLink from "next/link";
@@ -18,6 +19,7 @@ import { TiArrowForward } from "react-icons/ti";
 import { RxDotsHorizontal } from "react-icons/rx";
 import { Video } from "@/components/Video/Video";
 import { useMediaQuery } from "@chakra-ui/react";
+import { Suspense } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
@@ -29,93 +31,102 @@ export default function Page() {
   const [isLargerThan1000] = useMediaQuery("(min-width: 1000px)");
 
   return (
-    <Flex flexDirection={isLargerThan1000 ? "row" : "column"}>
-      <Flex
-        justifyContent="center"
-        flexDirection="column"
-        padding="10px 20px"
-        w={{ base: "500px", md: "700px", lg: "1000px" }}
-        margin="0 auto"
-      >
-        <VideoPlayer videoId={videoId} title={snippetVideo?.title || ""} />
-        <Text fontWeight="bold" fontSize="x-large" margin="12px 0">
-          {snippetVideo?.title || ""}
-        </Text>
+    <Suspense fallback={<Skeleton />}>
+      <Flex flexDirection={isLargerThan1000 ? "row" : "column"}>
         <Flex
-          flexDirection="row"
-          justifyContent="space-between"
-          flexWrap="wrap"
+          justifyContent="center"
+          flexDirection="column"
+          padding="10px 20px"
+          w={{ base: "500px", md: "700px", lg: "1000px" }}
+          margin="0 auto"
         >
-          <Flex flexDirection="row" gap="12px">
-            <Link as={NextLink} href="https://www.youtube.com/@cbssportsgolazo">
-              <Image
-                src="https://yt3.ggpht.com/EKBT19BrrkDkhoxOjenonvDxIf0s4wDljOnCzPos7z3qAbmwA_cgNCDY_a1i_K_3dhH1BWmknQ=s240-c-k-c0x00ffffff-no-rj"
-                alt="image channel"
-                borderRadius="full"
-                boxSize="45px"
+          <VideoPlayer videoId={videoId} title={snippetVideo?.title || ""} />
+          <Text fontWeight="bold" fontSize="x-large" margin="12px 0">
+            {snippetVideo?.title || ""}
+          </Text>
+          <Flex
+            flexDirection="row"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Flex flexDirection="row" gap="12px">
+              <Link
+                as={NextLink}
+                href="https://www.youtube.com/@cbssportsgolazo"
+              >
+                <Image
+                  src="https://yt3.ggpht.com/EKBT19BrrkDkhoxOjenonvDxIf0s4wDljOnCzPos7z3qAbmwA_cgNCDY_a1i_K_3dhH1BWmknQ=s240-c-k-c0x00ffffff-no-rj"
+                  alt="image channel"
+                  borderRadius="full"
+                  boxSize="45px"
+                />
+              </Link>
+              <Flex flexDirection="column">
+                <Text>{snippetVideo?.channelTitle}</Text>
+                <Text fontSize="8px">5 K Suscriptores</Text>
+              </Flex>
+            </Flex>
+            <Flex gap="10px">
+              <Button variant="solid" color="black" background="#f2f2f2">
+                Unirme
+              </Button>
+              <Button color="white" background="black">
+                Suscribirme
+              </Button>
+            </Flex>
+            <Flex
+              flexGrow="0.2"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Button
+                rightIcon={<LuThumbsUp />}
+                aria-label="thumbs up"
+                size="sm"
+              >
+                {videoById?.statistics.likeCount}
+              </Button>
+              <Button
+                leftIcon={<LuThumbsDown />}
+                aria-label="thumbs down"
+                size="sm"
               />
-            </Link>
-            <Flex flexDirection="column">
-              <Text>{snippetVideo?.channelTitle}</Text>
-              <Text fontSize="8px">5 K Suscriptores</Text>
+              <IconButton as={TiArrowForward} aria-label="share">
+                Compartir
+              </IconButton>
+              <IconButton
+                aria-label="more options button"
+                as={RxDotsHorizontal}
+                isRound={true}
+              />
             </Flex>
           </Flex>
-          <Flex gap="10px">
-            <Button variant="solid" color="black" background="#f2f2f2">
-              Unirme
-            </Button>
-            <Button color="white" background="black">
-              Suscribirme
-            </Button>
-          </Flex>
-          <Flex
-            flexGrow="0.2"
-            justifyContent="space-between"
-            alignItems="center"
+          <Badge
+            whiteSpace="pre-wrap"
+            color="black"
+            background="#f2f2f2"
+            borderRadius="5%"
+            marginTop="20px"
+            padding="10px"
+            overflow="hidden"
+            textOverflow="ellipsis"
           >
-            <Button rightIcon={<LuThumbsUp />} aria-label="thumbs up" size="sm">
-              {videoById?.statistics.likeCount}
-            </Button>
-            <Button
-              leftIcon={<LuThumbsDown />}
-              aria-label="thumbs down"
-              size="sm"
-            />
-            <IconButton as={TiArrowForward} aria-label="share">
-              Compartir
-            </IconButton>
-            <IconButton
-              aria-label="more options button"
-              as={RxDotsHorizontal}
-              isRound={true}
-            />
-          </Flex>
+            {statisticsVideo?.viewCount || ""} visualizaciones <br />
+            {snippetVideo?.description}
+          </Badge>
         </Flex>
-        <Badge
-          whiteSpace="pre-wrap"
-          color="black"
-          background="#f2f2f2"
-          borderRadius="5%"
-          marginTop="20px"
-          padding="10px"
-          overflow="hidden"
-          textOverflow="ellipsis"
-        >
-          {statisticsVideo?.viewCount || ""} visualizaciones <br />
-          {snippetVideo?.description}
-        </Badge>
+        <Flex flexDirection="column" padding="10px 20px" gap="12px">
+          {video?.items.map((item) => {
+            return (
+              <Video
+                key={self.crypto.randomUUID()}
+                video={item}
+                isSuggested={true}
+              />
+            );
+          })}
+        </Flex>
       </Flex>
-      <Flex flexDirection="column" padding="10px 20px" gap="12px">
-        {video?.items.map((item) => {
-          return (
-            <Video
-              key={self.crypto.randomUUID()}
-              video={item}
-              isSuggested={true}
-            />
-          );
-        })}
-      </Flex>
-    </Flex>
+    </Suspense>
   );
 }

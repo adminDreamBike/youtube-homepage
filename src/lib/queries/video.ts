4178ed1a9e6b3df73cb201242cf510474fd3973f
carlos Rodriguez <carlosdumar4@gmail.com>
2025-06-05 16:00:35 -0500

    feat: react-query variables added

--- src/lib/queries/video.ts
+++ src/lib/queries/video.ts
@@ -3,10 +3,10 @@ import { fetchVideo } from "@/lib/api/video";
 import { IVideo } from "../types";
 
 export const useVideos = ({ url, q }: IVideo) => {
-  const { data, isLoading, isError, error, refetch, isSuccess } = useQuery({
-    queryKey: ["videos"],
-    queryFn: async () => await fetchVideo({ url, q }),
+  const { data, isLoading, isError, error, isSuccess } = useQuery({
+    queryKey: ["videos", "items", url, q],
+    queryFn: () => fetchVideo({ url, q }),
   });
 
-  return { videos: data, isLoading, isError, error, refetch, isSuccess };
+  return { videos: data, isLoading, isError, error, isSuccess };
 };

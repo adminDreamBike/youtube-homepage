73819420d76a774da8e20fde32509fc7c9fdf7a2
carlos Rodriguez <carlosdumar4@gmail.com>
2025-06-06 11:46:49 -0500

    fix: filters are working as expected

--- src/stores/videos.ts
+++ src/stores/videos.ts
@@ -12,24 +12,23 @@ interface VideoState {
     resultsPerPage: number;
   };
 }
-interface VideoStoreState {
-  video: VideoState;
-  filteredVideos?: any;
-}
-
 interface Actions {
   setVideos: (video: VideoState) => void;
   getVideoById: (id: string) => Item | undefined;
-  setFilteredVideos: (filteredVideos: any) => void;
+  setSelectedCategoryId: (selectedCategoryId: string) => void;
+  setVideosByCategory: (categoryId: string) => void;
+  clearFilters: () => void;
 }
 
 export interface VideoStore {
   video: VideoState;
+  filteredVideosByCategory?: any;
   filteredVideos?: any;
+  selectedCategoryId: string | null;
   actions: Actions;
 }
 
-const INITIAL_STATE: VideoStoreState = {
+const INITIAL_STATE = {
   video: {
     kind: "",
     etag: "",
@@ -40,20 +39,48 @@ const INITIAL_STATE: VideoStoreState = {
       resultsPerPage: 0,
     },
   },
-  filteredVideos: undefined,
+  filteredVideosByCategory: undefined,
 };
 
 const useVideoStore = create<VideoStore>((set, get) => ({
   video: INITIAL_STATE.video,
-  filteredVideos: INITIAL_STATE.filteredVideos,
+  filteredVideosByCategory: null,
+  selectedCategoryId: "all",
   actions: {
     setVideos: (video) => set(() => ({ video })),
     getVideoById: (id) => get().video.items.find((item) => item.id === id),
-    setFilteredVideos: (filteredVideos) => set({ filteredVideos }),
+    setSelectedCategoryId: (categoryId) =>
+      set({ selectedCategoryId: categoryId }),
+    setVideosByCategory: (categoryId) => {
+      const { video } = get();
+      if (categoryId === "all") {
+        set({
+          filteredVideosByCategory: video.items,
+          selectedCategoryId: categoryId,
+        });
+        return;
+      }
+      const filteredVideos = video.items.filter(
+        (item) => item.snippet.categoryId === categoryId
+      );
+      set({
+        filteredVideosByCategory: filteredVideos,
+        selectedCategoryId: categoryId,
+      });
+    },
+    clearFilters: () =>
+      set({ filteredVideos: null, selectedCategoryId: "all" }),
   },
 }));
 
 export const useFilteredVideos = () =>
-  useVideoStore((state) => state.filteredVideos);
+  useVideoStore((state) => {
+    if (!state.selectedCategoryId || state.selectedCategoryId === "all") {
+      return state.video.items;
+    }
+    return state.filteredVideosByCategory;
+  });
+export const useSelectedCategoryId = () =>
+  useVideoStore((state) => state.selectedCategoryId);
 export const useVideoActions = () => useVideoStore((state) => state.actions);
 export const useVideos = () => useVideoStore((state) => state.video);
